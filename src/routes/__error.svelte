243788c47cec5b1a5a97abcb78c4eafb67846992
Pedro Lunet <pedrolunet@significa.co>
2025-07-21 17:50:21 +0100

    error pages

---
+++ src/routes/__error.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error }) {
+		return {
+			props: { message: error.message },
+		};
+	}
+</script>
+
+<script>
+	export let message;
+</script>
+
+<div class="error">
+	<h2>Ooopsi</h2>
+	<p>{message}</p>
+	<a href="/">Go back to home</a>
+</div>
+
+<style>
+	.error {
+		text-align: center;
+		margin-bottom: 60px;
+	}
+</style>
