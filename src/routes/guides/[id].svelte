6bd50a7b95f584619e454c72006c19f0c27d5007
Pedro Lunet <pedrolunet@significa.co>
2025-07-21 17:38:12 +0100

    Dynamic Routes

---
+++ src/routes/guides/[id].svelte
@@ -0,0 +1,37 @@
+<script context="module">
+	export async function load({ fetch, page }) {
+		const id = page.params.id;
+		const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+		const guide = await res.json();
+
+		if (res.ok) {
+			return {
+				props: {
+					guide,
+				},
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error('Could not fetch the guide'),
+		};
+	}
+</script>
+
+<script>
+	export let guide;
+</script>
+
+<div class="guide">
+	<h1>{guide.title}</h1>
+	<p>{guide.body}</p>
+</div>
+
+<style>
+	.guide {
+		margin-top: 40px;
+		padding: 10px;
+		border: 1px solid rgba(255, 255, 255, 0.2);
+	}
+</style>
